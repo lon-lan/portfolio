a40110ebcf1525c84b1fb7123b67784ea8417e22
lon-lan <jhonlouietiongco@yahoo.com>
2026-08-01 19:28:03 +0800

    Portfolio

---
+++ scirpt.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll(".Bottom");
+
+hiddenElements.forEach((element) => {
+    observer.observe(element);
+});
